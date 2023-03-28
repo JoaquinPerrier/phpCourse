f304643aa2604fcc2e20432924468ea99786fa45
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-03-28 20:16:38 -0300

    Agregando variables y tipos de datos

--- index.php
+++ index.php
@@ -21,6 +21,60 @@
 
     print "Hello" . " people<br>"; // EL PUNTO CONCATENA EN print
 
+
+    // DECLARACIÓN DE VARIABLES:
+    // Tienen que empezar con una letra, un guion bajo. Seguido de un numero, una letra o guion bajo
+    $city = "Rosario";
+    echo "I live in " . $city . "<br>";
+
+    // DATE TYPES
+    // Integers:
+    $age = 24;
+    // Floating point or float
+    $price = 10.5;
+    // String
+    $surname = "Perrier";
+    // Boolean
+    $is_login = false;
+    // Array
+    $cars = array("Bwm","Audi","Mercedes");
+    $cars2 = array("Renoleta","VW","Rastrojero");
+    echo $cars[2]. "<br>"; // MUESTRA MERCEDES
+    $myCar = array("Audi", 2016, 60.123);
+
+    var_dump($myCar); // MUESTRA INFO DEL LA VARIABLE
+
+    echo "<br>";
+
+    print_r($myCar); // MUESTRA INFO DE CADA UNO DE LOS LUGARES DEL ARRAY
+
+    echo "<br>";
+
+    $allCars = array_merge($cars,$cars2); // MERGEA 2 ARRAYS PARA CREAR UNO NUEVO
+    print_r($allCars);
+    echo "<br>";
+
+    // ASSOCIATE ARRAYS: Asocia un index (lo que está antes de la fecha) con un valor (lo que está después de la flecha)
+    $associateCars = array("Audi" => 50.000,"Alfa" => 1.200,"Reno" => 34.000);
+    var_dump($associateCars);
+    echo $associateCars["Audi"]. "<br>"; // MUESTRA MERCEDES
+    foreach($associateCars as $key => $value){
+        echo "My " . $key . " has ". $value . " kilometers <br>";
+    }
+
+    // Multidimensional arrays
+    $dobleArrays = array("Expensives"=>array("Bmw","Audi","Alfa romeo"),"Cheaps"=>array("Reno", "Susuki","Fiat"));
+    echo $dobleArrays["Expensives"][0] . "<br>"; // SE INDICA ARRAY ASOCIADO Y DSP EL INDEX DEL ARRAY QUE ESTA ADENTRO
+    echo $dobleArrays["Cheaps"][0] . "<br>"; // SE INDICA ARRAY ASOCIADO Y DSP EL INDEX DEL ARRAY QUE ESTA ADENTRO
+
+
+    // Object
+
+    // Resource
+
+    // Null
+    $X = null;
+
     ?>
 </body>
 </html>
