318bdbd02e1b7326dbee11cdef0059ec4a0de9bf
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-03-29 12:48:49 -0300

    Agregando operadores aritmeticos y de asignacion

---
+++ operators/index.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // ARITMETICOS
+    // Suma
+    $suma = 1 + 2;
+    // Resta
+    $resta = 10 - 3;
+    // Multiplicacion
+    $multiplicacion = 2 * 3;
+    // Division
+    $division = 10 / 2;
+    // Resto
+    $resto = 25 % 5;
+
+    echo "$suma, $resta, $multiplicacion, $division, $resto <br>";
+
+    // OPERADORES DE ASIGNACION
+    $x = 5;
+    echo "$x <br>";
+    // Suma y asignacion
+    $x += 5;
+    echo "$x <br>";
+    // Resta y asignacion
+    $x -= 1;
+    echo "$x <br>";
+    // Multiplicacion y asignacion
+    $x *= 5;
+    echo "$x <br>";
+    // Division y asignacion
+    $x /= 10;
+    echo "$x <br>";
+    // Concatena y asigna
+
+    ?>
+</body>
+</html>
