d1f294b7da335eb68a6c5faab8441d57d59b1bdc
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-03-29 14:27:30 -0300

    Agregando operadores de comparacion y lógicos

--- operators/index.php
+++ operators/index.php
@@ -37,7 +37,57 @@
     $x /= 10;
     echo "$x <br>";
     // Concatena y asigna
+    $string = "Alojaaa";
+    $string .= " Como andan muchachos?";
+    echo " $string <br>";
 
+    // OPERADORES DE COMPARACION
+    $num1 = 10;
+    $num2 = 13;
+    // Menor o menor/igual
+    $num1 < $num2;
+    // Mayor o mayor/igual
+    $num1 >= $num2;
+    // Igual
+    $num1 == $num2;
+    // Distinto
+    $num1 != $num2;
+    $num1 <> $num2;
+    // Ídenticos
+    $num1 === $num2;
+    // Spaceship operator  
+    // Si el valor de la izquierda es menor al de la derecha, devuelve un -1
+    // Si los valores son iguales, devuelve un 0
+    // Si el valor de la derecha es menor al de la izquierda, devuelve un 1.
+    $num1 <=> $num2;
+
+    // OPERADORES LOGICOS
+    $x = 0;
+    $y = 10;
+    // and &&
+    if ($x != $y and 2 < 3) {
+        echo "True! <br>";
+    } else {
+        echo "False! <br>";
+    }
+    // or ||
+    if ($x == $y || 2 < 3) {
+        echo "True! <br>";
+    } else {
+        echo "False! <br>";
+    }
+    // xor  UNO ES TRUE, PERO NO LOS DOS
+    if ($x != $y xor 2 < 3) {
+        echo "True! <br>";
+    } else {
+        echo "False! <br>";
+    }
+    // ! NEGACION
+    if (!$x != $y xor 2 < 3) {
+        echo "True! <br>";
+    } else {
+        echo "False! <br>";
+    }
     ?>
 </body>
 </html>
