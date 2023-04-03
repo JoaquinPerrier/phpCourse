f58f2e49abd5770d8b5a313ce95e525a48fdf1a6
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-04-03 14:53:24 -0300

    Iniciando build in functions de php

--- import&require/index.php
+++ import&require/index.php
@@ -1,4 +1,6 @@
 <?php
+    // INCLUDE: si el path está mal, solo nos envía un mensaje de alerta
+    // REQUIRE: si el path está mal, manda un fatal error
     include 'includes/head.php';
     include 'includes/header.php';
 ?>
