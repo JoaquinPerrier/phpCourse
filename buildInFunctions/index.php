84cd3230c63b068f8c195533c30a6a9ca0ae78ad
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-04-03 19:05:24 -0300

    Agregando funciones matematicas y de string que posee php

--- buildInFunctions/index.php
+++ buildInFunctions/index.php
@@ -6,6 +6,47 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    // FUNCIONES MATEMÁTICAS
+    // Ceil: redondea un flotante al integer mayor
+    echo ceil(0.45)."<br>";
     
+    // Floor: redondea un flotante al integer menor
+    echo floor(0.45)."<br>";
+
+    // Round: redondea el número. El segundo parámetro es la cantidad de números dsp de la coma
+    echo round(0.65 , 0)."<br>";
+
+    // Exponencial: número e elevado al parámetro que le pases
+    echo exp(2)."<br>";
+
+    // FUNCIONES DE STRING
+    $str = "Today is a cloudy day";
+
+    // Cantidad de caracteres:
+    echo strlen($str). "<br>";
+
+    // Contador de palabras;
+    echo str_word_count($str) . "<br>";
+
+    // Encontrar texto en un string
+    if (strpos($str, "day")){
+        echo "this is a valid sentense <br>";
+    } else {
+        echo "this is a invalid sentense <br>";
+    }
+
+    // Ucwords: todas las primeras letras de las palabras se pasan a mayusculas
+    echo ucwords($str) . " <br>";
+
+    // Todo a mayusculas
+    echo strtoupper($str) . " <br>";
+
+    // Todo a minusculas
+    echo strtolower($str) . " <br>";
+
+
+
+    ?>
 </body>
 </html>
