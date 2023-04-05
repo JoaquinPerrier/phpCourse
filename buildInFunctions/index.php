7071446a240519ae67a8529845de48b1a83eec15
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-04-04 22:30:34 -0300

    Agregando funciones array y de fecha/hora

--- buildInFunctions/index.php
+++ buildInFunctions/index.php
@@ -45,6 +45,63 @@
     // Todo a minusculas
     echo strtolower($str) . " <br>";
 
+    // FUNCIONES DE ARRAY
+    $personalInfo = array ("Name"=>"Dary", "Age"=>24, "City"=> "amsterdan");
+    $moreInfo = array("State"=>"NH", "Weight"=> 85);
+
+    $personalInfo = array_merge($personalInfo, $moreInfo);
+
+    foreach ($personalInfo as $key => $value) {
+        echo $key . ": " . $value . "<br>";
+    }
+
+    // Imprime solo las keys
+    print_r (array_keys($personalInfo));
+    echo "<br>";
+
+    // Imprime solo los values
+    print_r (array_values($personalInfo));
+    echo "<br>";
+
+    // Agrega un valor al final
+    $cars = array ("Volkswagen", "Audi", "BMW");
+    array_push($cars,"Volvo");
+    print_r ($cars);
+    echo "<br>";
+
+
+    // Busca un valor dentro del array, y devuelve la key
+    echo array_search("BMW", $cars);
+    echo "<br>";
+
+    // Devuelve el número de elementos del array
+    echo count($cars);
+    echo "<br>";
+
+    // FUNCIONES DE FECHAS Y HORA
+    // FECHAS:
+    // d = IMPRIME DIA EN NUMEROS. j = IMPRIME DIA SIN CEROS. D = IMPRIME DIA EN LETRAS (PRIMERAS 3). l = IMPRIME EL DIA COMPLETO EN LETRAS.
+    // m = IMPRIME MES CON NUMEROS. n = IMPRIME MES SIN CEROS. M = IMRPRIME MES EN LETRAS (3 PRIMERAS). F = IMPRIME MES ENTERO
+    // y = IMPRIME EL AÑO CON 2 DÍGITOS. Y = IMPRIME EL AÑO COMPLETO
+    echo date('D') . "<br>";
+
+    echo date('j/n/Y') . "<br>";
+
+    // HORAS:
+    // g = horas en formato 12-horas sin ceros. h = horas en formato 12-horas con ceros. G = horas in 24-horas sin ceros. H = horas en formato 24-horas con ceros.
+    // a = am/pm. A = AM/PM
+    // i = minutos sin ceros adelante
+    // s = segundos sin ceros adelante.
+    
+    // Función para setear el timezone a Argentina
+    date_default_timezone_set('America/Argentina/Cordoba');
+
+    echo date('H:i:s A') . "<br>";
+
+    // Cadena de caracteres a hora
+    $time = strtotime('4:00pm December 03 2019');
+    echo $time . "<br>"; // Imprime un timestamp con los segundos desde el 1ro de enero de 1970. Se llama UNIX timestamp
+
 
 
     ?>
