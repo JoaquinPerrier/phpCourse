a5c297005a5f1e435f73e10772abf764fce48b98
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-04-10 19:52:28 -0300

    Agregando GLOBALS

--- superGlobals/index.php
+++ superGlobals/index.php
@@ -6,6 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+<?php
+    // Superglobals
+    // GLOBALS: VARIABLE ACCSIBLE DESDE CUALQUIER PARTE DEL CODIGO
+    $x = 100;
+    $y = 100;
+
+    function add() {
+        $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+    }
+
+    add();
+    echo $z;
+
+    // $_POST
+    // $_GET
+    // $_SERVER
+    // $_COOKIE
+    // $_SESSION
+    // $_FILES
+?>
 </body>
 </html>
