2198755a0c7b9849f0194ad66e230ffd3aa53e20
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-06-02 11:09:50 -0300

    Terminando operadores

--- CF/2operadores.php
+++ CF/2operadores.php
@@ -1,5 +1,5 @@
 <?php
-// OPERADORES
+// OPERADORES DE COMPARACIÓN
 // Comparación igual == 
 $raza = "Golden";
 var_dump($raza == "Labrador");
@@ -28,4 +28,73 @@ El primer valor es Mayor
 -1 El primer valor es menor*/
 
 var_dump($edad <=> 10); // DEVUELVE 1
+
+// OPERADORES ARITMETICOS
+// Suma +
+// Resta - 
+$numero = 10;
+$numero2 = 30;
+echo $numero + $numero2;
+echo $numero2 - $numero;
+
+// Multiplicación *
+// División /
+echo $numero2 * $numero;
+echo $numero2 / $numero;
+
+// Modulo %
+// Exponenciación **
+echo $numero2 % $numero;
+echo $numero2 ** 2;
+
+
+// OPERADORES DE ASIGNACIÓN
+// Operador de asignación = 
+// Operador de asignación por referencia = &: GUARDA EL MISMO LUGAR DE LA MEMORIA (SI CAMBIA a, TAMBIÉN SE GUARDA EN b)
+// Operador de asignación para concatenar .=
+$a = 10;
+$b = &$a;
+
+$saludo = "Hola ";
+$saludo .= "Cómo va?" // RESULTADO: Hola Cómo va?
+
+echo $b;
+
+// OPERADORES LÓGICOS
+// and &&
+$esMayor = $edad>=18;
+
+$regalo = "Si";
+$traeRegalo = $regalo == "Si";
+
+var_dump($esMayor && $traeRegalo);
+
+// or ||
+var_dump($esMayor || $traeRegalo);
+
+// xor (NO AMBOS VERDADEROS)
+var_dump($esMayor xor $traeRegalo);
+
+// no !
+var_dump(!$esMayor);
+
+// OPERADORES INCREMENTO Y DECREMENTO
+// Incremento ++
+// Decremento --
+$numero++;
+$numero2--;
+echo $numero . " " . $numero2;
+
+// OPERADOR TERNARIO ? : ;
+$esMayorDeEdad = $edad ? "Es mayor de edad":"Es menor de edad";
+echo $esMayorDeEdad;
+
+// OPERADOR ELVIS ?:
+$esMayorDeEdad = $edad ?: 5; // Si no posee valor, le da valor por default 5. TIENE QUE SER NULL
+
+// OPERADOR FUSION NULL ?? 
+$esMayorDeEdad = $edad ?? 5; // Es como el de arriba, pero permite variable sin nada
+
+
+
 ?>
